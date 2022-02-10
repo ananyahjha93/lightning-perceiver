d858f0a366fdd428e75380a8273ac4510096c992
ananyahjha93 <ananya@pytorchlightning.ai>
2022-02-10 15:52:56 -0500

    .

--- src/configs/image/classification/8_a100_adamw.sh
+++ src/configs/image/classification/8_a100_adamw.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 python src/tasks/images/classification.py \
+    --datamodule imagenet \
     --accelerator gpu \
     --num_accelerators 8 \
     --batch_size 16 \
     --num_workers 4 \
-    --optimizer adamw \
-    --scheduler warmup \
-    --learning_rate 0.0001 \
-    --weight_decay 0.05 \
-    --training_epochs 300 \
-    --warmup_epochs 5 \
+    --optimizer lamb \
+    --scheduler multi_step \
+    --learning_rate 0.000125 \
+    --weight_decay 0.1 \
+    --training_epochs 120 \
+    --milestones 84 102 114 \
+    --decay_coeff 0.1 \
     --data_dir /home/ananya/imagenet/data/imagenet_2012
